e8d15a0c0e0bbf1137a4473dabdab995f1aff114
Remo Inverardi <remo@inverardi.ch>
2025-07-07 23:00:00 +0200

    Implement the benchmark.

---
+++ benches/blockjack.rs
@@ -0,0 +1,53 @@
+use std::time::Duration;
+
+use blockjack::{naive_game::NaiveGame, secure_game::SecureGame};
+use blockjack::tfhe_keys::initialize_keys;
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+
+pub fn blockjack(criterion: &mut Criterion) {
+    let key = initialize_keys();
+
+    let mut group = criterion.benchmark_group("blockjack");
+
+    group.bench_function(BenchmarkId::new("playNaive", 0), |bench| {
+        bench.iter(|| {
+            let mut game = NaiveGame::new(0);
+
+            let deck = vec![6, 6, 6, 6, 6, 6];
+
+            game.plant_deck(&deck);
+            game.create_game();
+            game.hit_as_player();
+            game.stand();
+            game.hit_as_dealer();
+
+            game.dump_game();
+        })
+    });
+
+    group.bench_function(BenchmarkId::new("playSecure", 0), |bench| {
+        bench.iter(|| {
+            let mut game = SecureGame::new(&key);
+
+            let deck = vec![6, 6, 6, 6, 6, 6];
+
+            game.plant_deck(&deck);
+            game.create_game();
+            game.hit_as_player();
+            game.stand();
+            game.hit_as_dealer();
+
+            game.dump_game();
+        })
+    });
+
+    group.finish();
+}
+
+criterion_group! {
+    name = benches;
+    config = Criterion::default().measurement_time(Duration::from_secs(120)).sample_size(10);
+    targets = blockjack
+}
+
+criterion_main!(benches);
---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+pub mod naive_game;
+pub mod secure_game;
+pub mod tfhe_keys;
+pub mod tfhe_values;
